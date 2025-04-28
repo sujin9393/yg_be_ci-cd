5626dbb4f2fe919e9436b3554f6e999c18a67136
jieunyy <ina011011@naver.com>
2025-04-28 13:41:25 +0900

    feat: project init and directory added

---
+++ src/main/java/com/moogsan/moongsan_backend/MoongsanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moogsan.moongsan_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoongsanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoongsanBackendApplication.class, args);
+	}
+
+}
