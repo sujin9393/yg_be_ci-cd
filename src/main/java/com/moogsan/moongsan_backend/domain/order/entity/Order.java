bb86bfea882d066af6a66280d46bc6b4c6c13f60
khonetgithub <k_honet@naver.com>
2025-05-02 11:29:18 +0900

    feat/fix(order, groupbuy): feat order packages and files, fix GroupBuyCommandService

---
+++ src/main/java/com/moogsan/moongsan_backend/domain/order/entity/Order.java
@@ -0,0 +1,53 @@
+package com.moogsan.moongsan_backend.domain.order.entity;
+
+import com.moogsan.moongsan_backend.domain.groupbuy.entity.GroupBuy;
+import com.moogsan.moongsan_backend.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 유저 FK
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // 공구 FK
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private GroupBuy groupBuy;
+
+    @Column(length = 20, nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "deleted_count")
+    private Integer deletedCount = 0;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+}
