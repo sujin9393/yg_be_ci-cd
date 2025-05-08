17f3541718685e45faa743c5c4636b65a4d36608
khonetgithub <k_honet@naver.com>
2025-05-08 09:37:17 +0900

    fix(groupbuy, datasql): fix nickname, data

--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/dto/query/response/groupBuyDetail/UserProfileResponse.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/dto/query/response/groupBuyDetail/UserProfileResponse.java
@@ -9,7 +9,7 @@ public class UserProfileResponse {
 
     // 식별/메타
     private Long authorId;       // 작성자 아이디
-    private String nickName;       // 닉네임
+    private String nickname;       // 닉네임
 
     // 본문
     private String accountNumber;  // 계좌 번호
