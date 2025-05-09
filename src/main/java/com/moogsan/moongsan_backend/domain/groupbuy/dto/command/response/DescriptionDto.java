35eb770725f1a7e1f419373c72a6145104411dad
jieunyy <ina011011@naver.com>
2025-05-09 18:25:53 +0900

    feat(group-buy): change ApiResponse mapping

--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/dto/command/response/DescriptionDto.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/dto/command/response/DescriptionDto.java
@@ -3,21 +3,10 @@ package com.moogsan.moongsan_backend.domain.groupbuy.dto.command.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record DescriptionDto(
-        String message,
-        Data data
-) {
-    public record Data(
-            @JsonProperty("product_name")
-            String productName,
+        @JsonProperty("product_name") String productName,
+        @JsonProperty("product_lower_name") String productLowerName,
+        @JsonProperty("total_price") int totalPrice,
+        int count,
+        String summary
+) {}
 
-            @JsonProperty("product_lower_name")
-            String productLowerName,
-
-            @JsonProperty("total_price")
-            int totalPrice,
-
-            int count,
-
-            String summary
-    ) {}
-}
