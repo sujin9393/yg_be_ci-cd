ebc4dd042d3d1f009ab2be5b647bea0c987993fd
khonetgithub <k_honet@naver.com>
2025-05-10 23:53:46 +0900

    fix(order): delete ordercancel at order controller/service

--- src/main/java/com/moogsan/moongsan_backend/domain/order/controller/OrderController.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/order/controller/OrderController.java
@@ -5,12 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.moogsan.moongsan_backend.domain.order.dto.request.OrderCreateRequest;
 import com.moogsan.moongsan_backend.domain.order.service.OrderCreateService;
-import com.moogsan.moongsan_backend.domain.order.service.OrderCancelService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import jakarta.validation.Valid;
 import java.util.Map;
 import java.util.LinkedHashMap;
@@ -23,7 +20,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 public class OrderController {
 
     private final OrderCreateService orderCreateService;
-    private final OrderCancelService orderCancelService;
 
     @PostMapping("/orders")
     public ResponseEntity<?> createOrder(
@@ -37,17 +33,4 @@ public class OrderController {
         response.put("data", null);
         return ResponseEntity.status(201).body(response);
     }
-
-    // 주문 취소는 공구 상세 페이지에서 진행
-//    @DeleteMapping("/orders/postId/{postId}")
-//    public ResponseEntity<?> cancelOrder(
-//            @PathVariable Long postId,
-//            @AuthenticationPrincipal CustomUserDetails userDetails) {
-//
-//        orderCancelService.cancelOrder(userDetails.getUser().getId(), postId);
-//        Map<String, Object> response = new LinkedHashMap<>();
-//        response.put("message", "주문이 성공적으로 취소되었습니다.");
-//        response.put("data", null);
-//        return ResponseEntity.ok().body(response);
-//    }
 }
