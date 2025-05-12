4b687bda0657c77def0d0537051ef0a1e834d05b
jieunyy <ina011011@naver.com>
2025-05-12 16:32:22 +0900

    feat(group-buy): update ai description dto (#10)

--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/dto/command/response/DescriptionDto.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/dto/command/response/DescriptionDto.java
@@ -3,8 +3,8 @@ package com.moogsan.moongsan_backend.domain.groupbuy.dto.command.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record DescriptionDto(
+        @JsonProperty("title") String title,
         @JsonProperty("product_name") String productName,
-        @JsonProperty("product_lower_name") String productLowerName,
         @JsonProperty("total_price") int totalPrice,
         int count,
         String summary
