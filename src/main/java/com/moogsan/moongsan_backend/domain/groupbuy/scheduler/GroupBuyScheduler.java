d4896cf6dda427817030569ad6c580ae69d7d312
jieunyy <ina011011@naver.com>
2025-05-14 14:33:27 +0900

    feat(group-buy): change scheduler execution time (#10)

--- src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/scheduler/GroupBuyScheduler.java
+++ src/main/java/com/moogsan/moongsan_backend/domain/groupbuy/scheduler/GroupBuyScheduler.java
@@ -13,15 +13,15 @@ public class GroupBuyScheduler {
 
     private final GroupBuyCommandService groupBuyCommandService;
 
-    // 공구 게시글 dueDate 기반 자동 공구 마감 스케줄러(매일 정각에 작동)
-    @Scheduled(cron = "0 0 0 * * *")
+    // 공구 게시글 dueDate 기반 자동 공구 마감 스케줄러(매 정각(0분)과 30분에 작동)
+    @Scheduled(cron = "0 0/30 * * * *")
     public void closeExpiredGroupBuys() {
         LocalDateTime now = LocalDateTime.now();
         groupBuyCommandService.closePastDueGroupBuys(now);
     }
 
-    // 공구 게시글 pickupDate 기반 자동 공구 종료 스케줄러 (매일 01:00에 작동)
-    @Scheduled(cron = "0 0 1 * * *")
+    // 공구 게시글 pickupDate 기반 자동 공구 종료 스케줄러 (매 정각(0분)과 30분에 작동)
+    @Scheduled(cron = "0 0/30 * * * *")
     public void endPastPickupGroupBuys() {
         LocalDateTime now = LocalDateTime.now();
         groupBuyCommandService.endPastPickupGroupBuys(now);
